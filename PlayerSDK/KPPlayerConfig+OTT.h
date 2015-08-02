e1467db32e8ba09a87edeb51e146628060df35b8
Eliza Sapir <eliza.sapir@kaltura.com>
2015-08-02 15:16:28 +0300

    #FEC-3936 #comment attaching addConfigKey withDictionary + mini refactor on KPPlayerConfig

---
+++ PlayerSDK/KPPlayerConfig+OTT.h
@@ -0,0 +1,13 @@
+//
+//  KPPlayerConfig+OTT.h
+//  KALTURAPlayerSDK
+//
+//  Created by Eliza Sapir on 7/29/15.
+//  Copyright (c) 2015 Kaltura. All rights reserved.
+//
+
+#import "KPPlayerConfig.h"
+
+@interface KPPlayerConfig (OTT)
+
+@end
---
+++ PlayerSDK/KPPlayerConfig+OTT.m
@@ -0,0 +1,13 @@
+//
+//  KPPlayerConfig+OTT.m
+//  KALTURAPlayerSDK
+//
+//  Created by Eliza Sapir on 7/29/15.
+//  Copyright (c) 2015 Kaltura. All rights reserved.
+//
+
+#import "KPPlayerConfig+OTT.h"
+
+@implementation KPPlayerConfig (OTT)
+
+@end
