a4d835d73f256c6ce65fb4c7575aec1c5777262f
Eliza Sapir <eliza.sapir@kaltura.com>
2016-05-02 16:04:51 +0300

    #FEM-384 #comment add player instance property to KPViewController

--- KALTURAPlayerSDK/KPViewController.m
+++ KALTURAPlayerSDK/KPViewController.m
@@ -1110,6 +1110,18 @@ NSString *const KPErrorDomain = @"com.kaltura.player";
     KPLogTrace(@"Exit");
 }
 
+- (id<KPlayer>)player {
+    KPLogTrace(@"Enter");
+    
+    if (_playerFactory.player && [_playerFactory.player isMemberOfClass:[AVPlayer class]]) {
+        KPLogTrace(@"Exit");
+        return _playerFactory.player;
+    }
+
+    KPLogTrace(@"Exit::nil");
+    return nil;
+}
+
 @end
 
 
